2e42e94dc50f02231e8807375a2d639aecced5eb
Luke Leiter <ldlstarwars@gmail.com>
2018-01-17 11:15:51 -0500

    Input Events from Controllers
    *learned of six events, press up, press, press down; touch up, touch, and touch down
    *added a debug log to identify when these events occur

---
+++ Vive Mini Course/Assets/Scripts/PickupParent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+    // Use this for initialization
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Touch' the Trigger");
+        }
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'TouchDown' the Trigger");
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'TouchUp' the Trigger");
+        }
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Press' the Trigger");
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'PressDown' the Trigger");
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'PressUp' the Trigger");
+        }
+    }
+}
