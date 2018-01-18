1c32978da1c8bc599014902f11191877904da03a
Luke Leiter <ldlstarwars@gmail.com>
2018-01-18 13:15:00 -0500

    Parent the transform, and throw the object
    *connected the object to the controllers transform
    *gave the object the controllers velocities
    *reset the controller

--- Vive Mini Course/Assets/Scripts/PickupParent.cs
+++ Vive Mini Course/Assets/Scripts/PickupParent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(SteamVR_TrackedObject))]
@@ -6,6 +7,8 @@ public class PickupParent : MonoBehaviour {
 
     SteamVR_TrackedObject trackedObj;
     SteamVR_Controller.Device device;
+
+    public Transform sphere;
     // Use this for initialization
     void Awake () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
@@ -37,6 +40,50 @@ public class PickupParent : MonoBehaviour {
         if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
         {
             Debug.Log("You are holding 'PressUp' the Trigger");
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("You are holding 'PressUp' the Touchpad");
+            sphere.transform.position = Vector3.zero;
+            //sphere.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            //sphere.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        Debug.Log("You have collided with " + col.name + " And activated OnTriggerStay");
+        if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have collided with " + col.name + " while holding down Touch");
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(gameObject.transform);
+
+        }
+        if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have released Touch while colliding wih " + col.name);
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+
+            tossObject(col.attachedRigidbody);
+
+            }
+ 
+        }
+
+    void tossObject(Rigidbody rigidbody)
+    {
+        Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigidbody.velocity = origin.TransformVector(device.velocity);
+            rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+        }
+        else
+        {
+            rigidbody.velocity = device.velocity;
+            rigidbody.angularVelocity = device.angularVelocity;
         }
     }
 }
